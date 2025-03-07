095e93933281a0ef5007a8f78a17429687d13aff
Christian <christianlessamazutti@gmail.com>
2025-03-06 23:18:55 -0300

    music

--- code/Game.py
+++ code/Game.py
@@ -1,16 +1,17 @@
 import pygame
 
 from code.Const import WIN_HEIGHT, WIN_WIDTH
-from code.menu import Menu
+from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
-        self.window = None
         pygame.init()
         self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
+        pygame.mixer_music.load('.asset/Menu.mp3')
+        pygame.mixer_music.play(-1)
         while True:
             menu = Menu(self.window)
             menu.run()
