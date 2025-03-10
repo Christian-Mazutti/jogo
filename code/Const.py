cc80c3024e85271da8bab888740cdc686dfe50b5
Christian <christianlessamazutti@gmail.com>
2025-03-10 20:41:47 -0300

    menu

--- code/Const.py
+++ code/Const.py
@@ -1,4 +1,9 @@
-from pygame.examples.grid import WINDOW_WIDTH
-
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+# W
 WIN_WIDTH = 850
 WIN_HEIGHT = 480
